bb1e1bcfebdcdd951ef9ad06c75de90ee0f22f5b
Jehoshaphat Obol <86023492+Jehoshaphat-Obol@users.noreply.github.com>
2024-03-26 15:38:44 +0300

    initial release

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg|jpg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ]
+    }
+};
